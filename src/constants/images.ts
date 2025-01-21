249c236297f4917f3e2f21c9d4d9e949c9a76b9e
mikewegele <mike.wegele@fu-berlin.de>
2025-01-21 20:36:17 +0100

    adjust image paths

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,7 +1,7 @@
-export const FAMILY_HOUSE_IMAGE_URL = "https://png.pngtree.com/png-vector/20240531/ourlarge/pngtree-a-little-3d-house-png-image_12584924.png";
-export const SKYSCRAPER_IMAGE_URL = "https://static.vecteezy.com/system/resources/thumbnails/015/382/628/small/city-sky-tower-icon-isometric-style-vector.jpg";
+export const FAMILY_HOUSE_IMAGE_URL = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR89KgH5GTUlbPJU_I-xS3wf80e0_kYKD7huOnbsFJRpexA53qS";
+export const SKYSCRAPER_IMAGE_URL = "https://static.vecteezy.com/ti/gratis-vektor/p1/47046327-modern-buro-gebaude-stehen-hoch-mit-viele-fenster-darstellen-stadtisch-geschaft-center-vektor.jpg";
 export const COAL_POWER_PLANT_IMAGE_URL = "https://static.vecteezy.com/ti/gratis-vektor/t2/15913516-recycling-fabrik-symbol-isometrischer-stil-vektor.jpg";
-export const NUCLEAR_ENERGY_IMAGE_URL = "https://p.turbosquid.com/ts-thumb/w9/jRhrHk/uOMl4j9b/search/png/1596181261/600x600/fit_q87/2cf356647002d3fd51b34a72b28f7041803202f9/search.jpg";
+export const NUCLEAR_ENERGY_IMAGE_URL = "https://elements-resized.envatousercontent.com/elements-cover-images/2925c862-a533-42f2-8c7c-3553a38dda0e?w=433&cf_fit=scale-down&q=85&format=auto&s=225cbfb385162f62eeb16da3a5b55bf362c419617da1e78ed1cb0cb19cb6a1db";
 export const WINDMILL_IMAGE_URL = "https://img.freepik.com/psd-premium/3d-windturbine-gruene-energie-saubere-energie-umwelt-alternative-energie-konzept_112554-1169.jpg?semt=ais_hybrid";
 export const SOLAR_PANEL_IMAGE_URL = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSBNfN6_zONRQQ6TG9LdvTe3TkJb1qK9sNqEgdkQeUkYHOt4IXL";
-export const BATTERY_IMAGE_URL = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS54KB4A3m88Wy0b0q107mUuWTAwpz-XhvH8rJP1slj4BF5xbJv";
+export const BATTERY_IMAGE_URL = "https://png.pngtree.com/png-vector/20240203/ourmid/pngtree-illustration-3d-battery-power-with-charge-level-indicator-electrical-energy-accumulator-png-image_11534181.png";
